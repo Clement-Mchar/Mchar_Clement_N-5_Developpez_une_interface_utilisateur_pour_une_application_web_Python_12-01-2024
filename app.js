d546977c40abd8dc16f7b0edf16e4c1854f651e3
Clement-Mchar <clement.mchar@gmail.com>
2024-01-23 21:21:48 +0100

    Ajout des images des films des carousels

--- app.js
+++ app.js
@@ -54,3 +54,47 @@ setupCarousel('.category.action');
 setupCarousel('.category.adventure');
 setupCarousel('.category.comedy');
 
+async function fetchAndInsertImages(apiUrl, carouselSelector) {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        const carousel = document.querySelector(carouselSelector);
+        const images = [];
+
+        for (let i = 0; i < 7 && i < data.results.length; i++) {
+            const movie = data.results[i];
+            const movieId = movie.id; 
+            images.push(`<li class="item" title="Film 5" data-id="${movieId}">
+                            <a class="btn" href="#modal">
+                                <img src="${movie.image_url}" alt="Film ${i + 1}">
+                            </a>
+                        </li>`);
+        }
+
+        if (images.length < 7 && data.next) {
+            const nextPageResponse = await fetch(data.next);
+            const nextPageData = await nextPageResponse.json();
+
+            for (let i = 0; images.length < 7 && i < nextPageData.results.length; i++) {
+                const movie = nextPageData.results[i];
+                const movieId = movie.id; 
+                images.push(`<li class="item" title="Film 5" data-id="${movieId}">
+                            <a class="btn" href="#modal">
+                                <img src="${movie.image_url}" alt="Film ${i + 1}">
+                            </a>
+                        </li>`);
+            }
+        }
+
+        carousel.innerHTML = images.join('');
+    } catch (error) {
+        console.error('Erreur lors de la récupération des images :', error);
+    }
+}
+
+fetchAndInsertImages("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=", '.category.best-rated .carousel');
+fetchAndInsertImages("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Action&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=", '.category.action .carousel');
+fetchAndInsertImages("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Adventure&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=", '.category.adventure .carousel');
+fetchAndInsertImages("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Comedy&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=", '.category.comedy .carousel');
+
