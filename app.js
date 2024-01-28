6d0dd2154e0507bb615c7d5908f55ca7a70d539b
Clement-Mchar <clement.mchar@gmail.com>
2024-01-29 00:19:19 +0100

    ajout des requêtes de la modale et du film en vedette, refactorisation

--- app.js
+++ app.js
@@ -18,12 +18,17 @@ document.addEventListener('DOMContentLoaded', function() {
                     const featuredMovieUrl = data.results[0].url;
 
                     fetch(featuredMovieUrl)
-                    .then(secondResponse => secondResponse.json())
-                    .then(movieData => {
-                        const movieSummary = movieData.description;
-                        const featuredMovieSummary = document.querySelector('.summary');
-                        featuredMovieSummary.textContent = movieSummary
-                    })
+                        .then(secondResponse => secondResponse.json())
+                        .then(movieData => {
+                            const movieSummary = movieData.description;
+                            const featuredMovieSummary = document.querySelector('.summary');
+                            const featuredMovie = document.querySelector('.featured-movie');
+                            const playButton = featuredMovie.querySelector('.featured-movie-layout .play-button');
+                            movieId = movieData.id;
+                            featuredMovie.dataset.id = movieId
+                            featuredMovieSummary.textContent = movieSummary;
+                            playButton.href = `#modal-${movieId}`;
+                        })
                 } else {
                     console.error("Aucun résultat trouvé dans la réponse de l'API.");
                 }
@@ -91,13 +96,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-
-
-    fetchAndInsertImages("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=", '.category.best-rated .carousel');
-    fetchAndInsertImages("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Action&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=", '.category.action .carousel');
-    fetchAndInsertImages("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Adventure&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=", '.category.adventure .carousel', 0);
-    fetchAndInsertImages("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Comedy&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=", '.category.comedy .carousel');
-
     async function doesImageExist(url) {
         try {
             const response = await fetch(url, { method: 'HEAD' });
@@ -106,6 +104,7 @@ document.addEventListener('DOMContentLoaded', function() {
             return false;
         }
     }
+
     function openModal() {
         let items = document.querySelectorAll('.carousel .item');
         const modal = document.querySelector('#modal')
@@ -117,11 +116,63 @@ document.addEventListener('DOMContentLoaded', function() {
                 
                 modal.setAttribute('id', `modal-${itemId}`)
                 modal.dataset.id = itemId;
+                displayMovieInfos(itemId);
             });
         };
+        let playButton = document.querySelector('.featured-movie-layout .play-button');
+        playButton.addEventListener('click', () => {
+
+            let featuredMovie = document.querySelector('.featured-movie');
+            let featuredMovieId = featuredMovie.dataset.id
+            
+            modal.setAttribute('id', `modal-${featuredMovieId}`);
+            modal.dataset.id = featuredMovieId;
+            displayMovieInfos(featuredMovieId);
+        }
+    );
         
     }
+    function displayMovieInfos(modalId) {
 
+        const modal = document.querySelector('.modal');
+
+        if (!modalId) {
+            console.error('L\'élément .modal n\'a pas d\'attribut data-id défini.');
+            return;
+        }
+        const movieUrl = `http://localhost:8000/api/v1/titles/${modalId}`;
+
+        fetch(movieUrl)
+            .then(response => response.json())
+            .then(movieData => {
+                const movieImage = modal.querySelector('.movie-image img')
+                const movieTitle = modal.querySelector('#movie-title-content');
+                const movieGenre = modal.querySelector('#movie-genre-content');
+                const movieReleaseDate = modal.querySelector('#movie-release-date-content');
+                const movieRating = modal.querySelector('#movie-rating-content');
+                const movieImdb = modal.querySelector('#movie-imdb-score-content');
+                const movieDirector = modal.querySelector('#movie-director-content');
+                const movieCast = modal.querySelector('#movie-cast-content');
+                const movieDuration = modal.querySelector('#movie-duration-content');
+                const movieCountry = modal.querySelector('#movie-country-content');
+                const movieBoxOffice = modal.querySelector('#movie-box-office-content');
+                const movieSummary = modal.querySelector('#movie-summary-content');
+
+                movieImage.getAttribute('src');
+                movieImage.src = movieData.image_url;
+                movieTitle.textContent = movieData.title;
+                movieGenre.textContent = movieData.genres;
+                movieReleaseDate.textContent = movieData.date_published;
+                movieRating.textContent = movieData.rated;
+                movieImdb.textContent = movieData.imdb_score;
+                movieDirector.textContent = movieData.directors;
+                movieCast.textContent = movieData.actors;
+                movieDuration.textContent = movieData.duration;
+                movieCountry.textContent = movieData.countries;
+                movieBoxOffice.textContent = movieData.worldwide_gross_income;
+                movieSummary.textContent = movieData.long_description;
+            })
+    }
     openModal()
 
     function closeModal(){
@@ -132,4 +183,8 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
     closeModal()
+    fetchAndInsertImages("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=", '.category.best-rated .carousel');
+    fetchAndInsertImages("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Action&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=", '.category.action .carousel');
+    fetchAndInsertImages("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Adventure&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=", '.category.adventure .carousel', 0);
+    fetchAndInsertImages("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Comedy&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=", '.category.comedy .carousel');
 });
